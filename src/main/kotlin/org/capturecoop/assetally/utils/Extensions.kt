f9ab7820f87b7252f93a9c2896fea6dd8acd3e21
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2024-08-26 23:43:59 +0200

    Small code cleanup

--- src/main/kotlin/org/capturecoop/assetally/utils/Extensions.kt
+++ src/main/kotlin/org/capturecoop/assetally/utils/Extensions.kt
@@ -3,8 +3,9 @@ package org.capturecoop.assetally.utils
 import io.ktor.http.content.*
 
 class ReadMultiPart(private val list: List<PartData>) {
-    fun getFormItem(name: String) = list.firstOrNull { it.name == name } as PartData.FormItem?
-    fun getFileItem(name: String) = list.firstOrNull { it.name == name } as PartData.FileItem?
+    fun getItem(name: String) = list.firstOrNull { it.name == name }
+    fun getFormItem(name: String) = getItem(name) as PartData.FormItem?
+    fun getFileItem(name: String) = getItem(name) as PartData.FileItem?
 }
 
 suspend fun MultiPartData.read() = ReadMultiPart(readAllParts())
